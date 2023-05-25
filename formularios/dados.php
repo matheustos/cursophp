b1aa2886236ef1b17bec9e7a0d0ab51cca9a3d0a
Matheus <matheusstos123456@gmail.com>
2023-05-25 20:18:09 -0300

    Atualizações

--- formularios/dados.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$nome = $_GET['nome'];
-$email = $_GET['email'];
-
-echo "Seu nome é $nome e seu email é $email";
-
-?>
--- formularios/index.php
+++ formularios/index.php
@@ -7,10 +7,87 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="dados.php" method="GET">
-        Nome: <input type="text" name="nome"><br>
+    <?php 
+    if(isset($_POST['enviar-formulario'])):
+        $erros = array();
+        // validação idade
+        if(!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
+            $erros[] = "Idade precisa ser um inteiro<br>";
+        endif;
+
+        if(!empty($erros)):
+            foreach($erros as $erro):
+                echo "<li> $erro </li>";
+            endforeach;
+        else: 
+            echo "Formato de idade válido!<br>";
+        endif;
+
+        //validação email
+        if(!$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)):
+            $erros[] = "Formato inválido";
+        endif;
+
+        if(!empty($erros)):
+            foreach($erros as $erro):
+                echo "<li> $erro </li>";
+            endforeach;
+        else: 
+            echo "Formato de email ok!";
+        endif;
+
+        //validação peso
+        if(!$peso = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT)):
+            $erros[] = "Peso precisa ser um número decimal<br>";
+        endif;
+
+        if(!empty($erros)):
+            foreach($erros as $erro):
+                echo "<li> $erro </li>";
+            endforeach;
+        else: 
+            echo "Formato de peso válido<br>";
+        endif;
+
+        //validação ip
+        if(!$ip = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP)):
+            $erros[] = "IP inválido<br>";
+        endif;
+
+        if(!empty($erros)):
+            foreach($erros as $erro):
+                echo "<li> $erro </li>";
+            endforeach;
+        else: 
+            echo "Formato de IP válido!<br>";
+        endif;
+
+        //validação url
+
+        if(!$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL)):
+            $erros[] = "Formato de url inválido<br>";
+        endif;
+
+        if(!empty($erros)):
+            foreach($erros as $erro):
+                echo "<li> $erro </li>";
+            endforeach;
+        else: 
+            echo "Formato de url ok!";
+        endif;
+    endif;
+    
+    
+    
+    
+    ?>
+    <form action="index.php" method="POST">
+        Idade: <input type="text" name="idade"><br>
         Email: <input type="email" name="email"><br>
-        <input type="submit" name="enviar">
+        Peso: <input type="text" name="peso"><br>
+        IP: <input type="text" name="ip"><br>
+        URL: <input type="text" name="url"><br>
+        <button type="submit" name="enviar-formulario"> Enviar </button><br>
     </form>
     
 </body>
